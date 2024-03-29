7fbe594a0d000084ceb147f7b2c76feee7029752
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-03-29 12:47:30 -0300

    splashscreen e tela galeria

---
+++ src/screens/Galeria.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Galeria() {
+  return (
+    <View>
+      <Text>Galeria</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
