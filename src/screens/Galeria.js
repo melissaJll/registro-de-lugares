8c816863920b6d708f6c701514c2980aecf1f159
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-03-29 20:05:00 -0300

    galeria ainda não funcionando

--- src/screens/Galeria.js
+++ src/screens/Galeria.js
@@ -1,12 +1,53 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import { getStorage, ref, listAll } from "firebase/storage";
 
 export default function Galeria() {
+  const [arquivos, setArquivos] = useState([]);
+
+  const storage = getStorage();
+  const imagesRef = ref(storage, "images");
+
+  useEffect(() => {
+    const listImages = async () => {
+      const imageList = await listAll(imagesRef);
+
+      const downloadURLs = [];
+      for await (const imageRef of imageList.items) {
+        const downloadURL = await getDownloadURL(imageRef);
+        downloadURLs.push(downloadURL);
+      }
+
+      setArquivos(downloadURLs);
+      console.log(downloadURLs);
+    };
+
+    listImages();
+  }, []);
+
   return (
-    <View>
-      <Text>Galeria</Text>
+    <View style={styles.container}>
+      {arquivos.map((downloadURL) => (
+        <Image
+          key={downloadURL}
+          source={{ uri: downloadURL }}
+          style={styles.imagem}
+        />
+      ))}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imagem: {
+    width: 200,
+    height: 200,
+    margin: 10,
+  },
+});
