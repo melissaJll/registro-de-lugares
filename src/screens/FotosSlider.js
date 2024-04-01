1f7e814840bf3320c6bc94913759f8cf06cc938b
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-04-01 10:41:46 -0300

    Update FotosSlider.js

--- src/screens/FotosSlider.js
+++ src/screens/FotosSlider.js
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View, Image, Text, FlatList } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  FlatList,
+  Pressable,
+} from "react-native";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 import SafeContainer from "../components/SafeContainer";
+import { AntDesign } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 export default function FotosSlider() {
-  // Estado para armazenar os dados das fotos
   const [arquivos, setArquivos] = useState([]);
+  const navigation = useNavigation();
 
   useEffect(() => {
     // Função para buscar as fotos no Firestore
@@ -13,9 +22,9 @@ export default function FotosSlider() {
       try {
         // Obtém uma referência para o Firestore
         const db = getFirestore();
-        // Obtém uma referência para a coleção "lugares" no Firestore
+        // obtém uma referência para a coleção "lugares" no Firestore
         const lugaresCollectionRef = collection(db, "lugares");
-        // Obtém os documentos da coleção "lugares"
+        // pega os documentos da coleção "lugares"
         const snapshot = await getDocs(lugaresCollectionRef);
         // Mapeia os documentos para extrair os dados das fotos
         const imageData = snapshot.docs.map((doc) => doc.data());
@@ -33,9 +42,7 @@ export default function FotosSlider() {
   // Função para renderizar cada item da lista de fotos
   const renderItem = ({ item }) => (
     <View style={estilos.item}>
-      {/* Exibe a descrição da foto */}
       <Text style={estilos.descricao}>{item.descricao}</Text>
-      {/* Exibe a imagem */}
       <Image source={{ uri: item.foto }} style={estilos.imagem} />
     </View>
   );
@@ -53,10 +60,11 @@ export default function FotosSlider() {
         />
       </View>
       <Pressable
-        style={estilos.botaoFotoGhost}
+        style={estilos.botaoIrGaleria}
         onPress={() => navigation.navigate("Galeria")}
       >
-        <Text style={estilos.botaoTextGhost}>Ver fotos</Text>
+        <Text style={estilos.textoBotaoGaleria}>Ver todas as fotos</Text>
+        <AntDesign name="arrowright" size={24} color="black" />
       </Pressable>
     </SafeContainer>
   );
@@ -65,13 +73,15 @@ export default function FotosSlider() {
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
   item: {
     // Aumenta a largura e do item para exibir imagens maiores
     width: 330,
     height: 650,
-    marginHorizontal: 20,
-    borderRadius: 16, // Adiciona um leve arredondamento nas bordas dos itens
+    marginHorizontal: 12,
+    borderRadius: 16,
     overflow: "hidden", // Esconde qualquer conteúdo que ultrapasse os limites do item
     position: "relative", // Definindo a posição como relativa para que a descrição fique sobre a imagem
   },
@@ -81,10 +91,31 @@ const estilos = StyleSheet.create({
     resizeMode: "cover", // Redimensiona a imagem para cobrir todo o espaço disponível
   },
   descricao: {
-    fontSize: 24, // Define o tamanho da fonte da descrição
-    paddingHorizontal: 16, // Adiciona um espaçamento horizontal interno à descrição
-    paddingVertical: 8, // Adiciona um espaçamento vertical interno à descrição
+    fontSize: 24,
+    paddingVertical: 8,
     fontWeight: "bold",
-    marginVertical: 20,
+    marginBottom: 5,
+    textAlign: "center",
+  },
+  textoBotaoGaleria: {
+    color: "#000", // Cor do texto alterada para branco
+    fontWeight: "500",
+    fontSize: 20,
+    textAlign: "center",
+    marginHorizontal: 4,
+  },
+  botaoIrGaleria: {
+    borderRadius: 12,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    marginVertical: 50,
+    marginHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 12,
+    borderColor: "#ECE2C8",
+    borderStyle: "solid",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
